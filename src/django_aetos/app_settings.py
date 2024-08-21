6090e659d0231d0c0caee9ab4a093cb447852e4d
Bokan Mohammad Assad <bokan@team.uber.space>
2024-08-21 12:35:57 +0200

    fix: settings as properties in `AppSettings` class
    to be able to change them during runtime, this is important for tests

--- src/django_aetos/app_settings.py
+++ src/django_aetos/app_settings.py
@@ -1,6 +1,31 @@
-from django.conf import settings
 
-ENABLE_IP_ALLOWLIST = getattr(settings, "AETOS_ENABLE_IP_ALLOWLIST", False)
-IP_ALLOWLIST = getattr(settings, "AETOS_IP_ALLOWLIST", [])
-ENABLE_AUTH = getattr(settings, "AETOS_ENABLE_AUTH", False)
-AUTH_TOKENLIST = getattr(settings, "AETOS_AUTH_TOKENLIST", [])
+class AppSettings:
+    def __init__(self, prefix):
+        self.prefix = prefix
+    
+    def _setting(self, name, dflt):
+        from django.conf import settings
+        
+        return getattr(settings, self.prefix + name, dflt)
+    
+    @property
+    def ENABLE_IP_ALLOWLIST(self):
+        return self._setting("ENABLE_IP_ALLOWLIST", False)
+
+    @property
+    def IP_ALLOWLIST(self):
+        return self._setting("IP_ALLOWLIST", [])
+    
+    @property
+    def ENABLE_AUTH(self):
+        return self._setting("ENABLE_AUTH", False)
+    
+    @property
+    def AUTH_TOKENLIST(self):
+        return self._setting("AUTH_TOKENLIST", [])
+
+_app_settings = AppSettings("AETOS_")
+
+def __getattr__(name):
+    # See https://peps.python.org/pep-0562/
+    return getattr(_app_settings, name)
